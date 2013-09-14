9c1c72eb978fb42a261b0b8ae61fae69e5755aaa
Petter Nyman <petter@pttr.se>
2013-09-14 12:32:29 +0200

    added empty settings

--- settings.js
+++ settings.js
@@ -1,6 +1,8 @@
-{
-    'amazon':{
-        'accessKeyId': "",
-        'secretAccessKey': ""
-    },
-}
+var config = {};
+
+config.amazon = {};
+
+config.amazon.accessKeyId = '';
+config.amazon.secretAccessKey = '';
+
+module.exports = config;
