8372d63988428131997c06199aa38bf46a37b455
Petter Nyman <petter@pttr.se>
2013-09-14 18:29:40 +0200

    save chunks

--- main.js
+++ main.js
@@ -3,11 +3,15 @@ var request = require('request');
 var router = require('./routes/main.js');
 
 http.createServer(function(req, res){
+  req.chunks = [];
+  req.on('data', function (chunk) {
+    req.chunks.push(chunk.toString());
+  });
 
-    router.dispatch(req, res, function(err){
-        if(err){
-            res.writeHead(500);
-            res.end();
-        }
-    });
+  router.dispatch(req, res, function(err){
+    if(err){
+      res.writeHead(500);
+      res.end();
+    }
+  });
 }).listen(8042,'0.0.0.0');
