01f2bb90577fdcd051d09057ffb0a173caf162d0
Petter Nyman <petter@pttr.se>
2013-09-14 15:51:37 +0200

    removed files

--- settings.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.amazon = {};
-
-config.amazon.accessKeyId = 'AKIAIYYC5MNPDYKFYX0Q';
-config.amazon.secretAccessKey = 'rI8ewebVd21PTnQp1zm72jndLmR3cgviqWaF13IP';
-
-module.exports = config;
