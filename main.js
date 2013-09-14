faaedc5d8780e38e570da07a586eeb3b1c1a82bb
peny <petter@pttr.se>
2013-09-14 18:16:46 +0000

    converted project to express becasue of streaming file uploads

--- main.js
+++ main.js
@@ -1,17 +1,42 @@
+var express = require('express');
+var router = require('./routes/main.js');
+var app = express();
+app.use(express.bodyParser());
+
+
+
+app.get('/appreciation', function(req, res){
+	router.renderIndex(req,res);
+});
+
+app.get('/uploads/*', function(req, res){
+	router.renderStaticFile(req,res);
+});
+
+app.post('/appreciation', function(req, res){
+	router.saveAppreciationRequest(req,res);
+});
+
+app.get('/', function(req, res){
+	router.renderIndex(req,res);
+});
+
+app.listen(8042);
+
+/*
 var http = require('http');
 var request = require('request');
 var router = require('./routes/main.js');
+var union = require('union');
 
-http.createServer(function(req, res){
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
-
-  router.dispatch(req, res, function(err){
-    if(err){
-      res.writeHead(500);
-      res.end();
-    }
-  });
-}).listen(8042,'0.0.0.0');
+var server = union.createServer({
+before: [	
+	function(req, res){
+	  var found = router.dispatch(req, res);
+		if(!found){
+	res.emit('next');
+}
+	}
+]
+});
+server.listen(8042);*/
--- routes/main.js
+++ routes/main.js
@@ -1,37 +1,48 @@
 var fs = require('fs');
-var director = require('director');
 
 var mechaturk = require('../lib/mechaturk.js');
 
-function renderIndex(){
+function renderIndex(req,res){
   var _this = this;
 
-  _this.res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
+  res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
     var data = {
         price: Math.floor(Math.random()*600),
     };
-    _this.res.end(JSON.stringify(data));
+    res.end(JSON.stringify(data));
 }
 
-function saveAppreciationRequest(){
+function renderStaticFile(req,res){
+	//THis is as dumb as it gets, I'm sorry.
+	console.log(req.path);
+	var filename = req.path.match(/\/[^\/]+$/);
+
+	
+	fs.readFile('uploads'+filename, function(err,file){
+console.log(err);
+		res.write(file);
+	});
+}
+
+function saveAppreciationRequest(req, res){
     var _this = this;
-    console.log(_this.res);
-    mechaturk.createHIT({});
-  _this.res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
-    var data = {
-        price: Math.floor(Math.random()*600),
-    };
-    _this.res.end('<h1>OK! YOU\'RE DONE</h1>');
-}
-
-var router = new director.http.Router({
-  '/': {
-    get: renderIndex
-  },
-  '/appreciation': {
-    get: renderIndex,
-    post: saveAppreciationRequest
-  }
+console.log(req.files);
+fs.readFile(req.files.images.path, function (err, data) {
+  var newPath = "uploads/"+req.files.images.name;
+  fs.writeFile(newPath, data, function (err) {
+  });
 });
 
-module.exports = router;
+    console.log(req);
+	console.log(req.files);
+    mechaturk.createHIT({});
+  res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
+    var data = {
+        price: Math.floor(Math.random()*600),
+    };
+   res.end('<h1>OK! YOU\'RE DONE</h1>');
+}
+
+module.exports.renderIndex = renderIndex;
+module.exports.saveAppreciationRequest = saveAppreciationRequest;
+module.exports.renderStaticFile = renderStaticFile;
