b206599cf7d248786eb53ac51d89c1c4625bdaae
Petter Nyman <petter@pttr.se>
2013-09-14 16:48:53 +0200

    removed everything not used from main.js

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ var router = require('./routes/main.js');
 
 http.createServer(function(req, res){
 
-  request('https://api.github.com/zen', function(error, response, title){
     router.dispatch(req, res, function(err){
         if(err){
             res.writeHead(500);
             res.end();
         }
     });
-  });
 }).listen(8042,'0.0.0.0');
