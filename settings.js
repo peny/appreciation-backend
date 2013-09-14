791baab5a754953bf6c48f6fd972e4756768f394
Petter <petter@pttr.se>
2013-09-14 15:43:59 +0200

    Delete settings.js

--- settings.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.amazon = {};
-
-config.amazon.accessKeyId = 'AKIAIYYC5MNPDYKFYXOQ';
-config.amazon.secretAccessKey = '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';
-
-module.exports = config;
