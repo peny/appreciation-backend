1c8bd12058a4f5c19aa4ceaa2f0fc2232f5a7c93
peny <petter@pttr.se>
2013-09-15 12:11:36 +0000

    create HITs correctly etc.

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,13 @@ var fs = require('fs');
 var mechaturk = require('../lib/mechaturk.js');
 
 function renderIndex(req,res){
-  var _this = this;
+	var _this = this;
 
-  res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
-    var data = {
-        price: Math.floor(Math.random()*600),
-    };
-    res.end(JSON.stringify(data));
+	res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
+	var data = {
+price: Math.floor(Math.random()*600),
+	};
+	res.end(JSON.stringify(data));
 }
 
 function renderStaticFile(req,res){
@@ -17,35 +17,33 @@ function renderStaticFile(req,res){
 	console.log(req.path);
 	var filename = req.path.match(/\/[^\/]+$/);
 
-	
+
 	fs.readFile('uploads'+filename, function(err,file){
-		console.log(err);
-		res.write(file);
-	});
+			console.log(err);
+			res.write(file);
+			});
 }
 
 function saveAppreciationRequest(req, res){
-    var _this = this;
-console.log(req.files);
-fs.readFile(req.files.images.path, function (err, data) {
-  var newPath = "uploads/"+req.files.images.name;
-  fs.writeFile(newPath, data, function (err) {
-  });
-});
-
-    console.log(req);
-	console.log(req.files);
-
+	var _this = this;
+	fs.readFile(req.files.images.path, function (err, data) {
+			var newPath = "uploads/"+req.files.images.name;
+			fs.writeFile(newPath, data, function (err) {
+				});
+			});
 	var data = {
-		imageurl: 'uploads/'+req.files.images.name
+	imageurl: 'uploads/'+req.files.images.name,
+	  accesstoken: req.body.accesstoken,
+	  storeid: req.body.storeid,
+	  storedashboardurl: req.body.storedashboardurl	
 	};
-console.log(data);
-    mechaturk.createHIT(data);
-  res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
-    var data = {
-        price: Math.floor(Math.random()*600),
-    };
-   res.end('<h1>OK! YOU\'RE DONE</h1>');
+	console.log(data);
+	mechaturk.createHIT(data);
+	res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
+	var data = {
+price: Math.floor(Math.random()*600),
+	};
+	res.end('<h1>OK! YOU\'RE DONE</h1>');
 }
 
 module.exports.renderIndex = renderIndex;
