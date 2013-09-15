7df1df652f71f152323039778f5ab7ca34d534f9
Petter Nyman <petter@pttr.se>
2013-09-15 14:58:30 +0200

    made main rotes indented etc.

--- routes/main.js
+++ routes/main.js
@@ -3,45 +3,45 @@ var fs = require('fs');
 var mechaturk = require('../lib/mechaturk.js');
 
 function renderIndex(req,res){
-	var _this = this;
+  var _this = this;
 
-	res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
-	var data = {
-price: Math.floor(Math.random()*600),
-	};
-	res.end(JSON.stringify(data));
+  res.writeHead(200,{'Content-Type': 'application/json; charset=utf8'});
+  var data = {
+    price: Math.floor(Math.random()*600),
+  };
+  res.end(JSON.stringify(data));
 }
 
 function renderStaticFile(req,res){
-	//THis is as dumb as it gets, I'm sorry.
-	console.log(req.path);
-	var filename = req.path.match(/\/[^\/]+$/);
+  //This is as dumb as it gets, I'm sorry.
+  console.log(req.path);
+  var filename = req.path.match(/\/[^\/]+$/);
 
 
-	fs.readFile('uploads'+filename, function(err,file){
-			console.log(err);
-			res.write(file);
-			});
+  fs.readFile('uploads'+filename, function(err,file){
+    console.log(err);
+    res.write(file);
+  });
 }
 
 function saveAppreciationRequest(req, res){
-	var _this = this;
-	fs.readFile(req.files.images.path, function (err, data) {
-			var newPath = "uploads/"+req.files.images.name;
-			fs.writeFile(newPath, data, function (err) {
-				});
-			});
-	var data = {
-	imageurl: 'uploads/'+req.files.images.name,
-	  accesstoken: req.body.accesstoken,
-	  storeid: req.body.storeid,
-	  storedashboardurl: req.body.storedashboardurl	
-	};
-	mechaturk.createHIT(data);
-	res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
-	fs.readFile('views/done.html', function(err, html){
-		res.end(html);
-	});
+  var _this = this;
+  fs.readFile(req.files.images.path, function (err, data) {
+    var newPath = "uploads/"+req.files.images.name;
+    fs.writeFile(newPath, data, function (err) {
+    });
+  });
+  var data = {
+    imageurl: 'uploads/'+req.files.images.name,
+    accesstoken: req.body.accesstoken,
+    storeid: req.body.storeid,
+    storedashboardurl: req.body.storedashboardurl
+  };
+  mechaturk.createHIT(data);
+  res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
+  fs.readFile('views/done.html', function(err, html){
+    res.end(html);
+  });
 }
 
 module.exports.renderIndex = renderIndex;
