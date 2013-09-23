a014b8637e8471772c711a4befdaa1c83c1d7356
Petter Nyman <petter@pttr.se>
2013-09-23 18:52:10 +0200

    removed home-coded static file serving and changed upload directory to media

--- routes/main.js
+++ routes/main.js
@@ -4,21 +4,10 @@ var mechaturk = require('../lib/mechaturk.js');
 var tictail = require('../lib/tictail.js');
 var db = require('../lib/db.js');
 
-function renderStaticFile(req,res){
-  //This is as dumb as it gets, I'm sorry.
-  console.log(req.path);
-  var filename = req.path.match(/\/[^\/]+$/);
-
-  fs.readFile('uploads'+filename, function(err,file){
-    console.log(err);
-    res.write(file);
-  });
-}
-
 function saveAppreciationRequest(req, res){
   var _this = this;
-	var filetype = req.files.images.name.match(/\..{2,5}$/);
-    var imagePath = "uploads/"+((new Date()).getTime())+filetype;
+  var filetype = req.files.images.name.match(/\..{2,5}$/);
+  var imagePath = "media/"+((new Date()).getTime())+filetype;
   fs.readFile(req.files.images.path, function (err, data) {
     fs.writeFile(imagePath, data, function (err) {
     });
@@ -31,7 +20,7 @@ function saveAppreciationRequest(req, res){
   };
   db.getQuota(data.storeid);
   tictail.getUser({accessToken: req.body.accesstoken}, function(err,user){
-	if(!err){
+    if(!err){
       mechaturk.createHIT(data);
       res.writeHead(200,{'Content-Type': 'text/html; charset=utf8'});
       fs.readFile('views/done.html', function(err, html){
