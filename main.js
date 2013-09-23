f98619865c9c7d80e744d8094e794e2468076de4
Petter Nyman <petter@pttr.se>
2013-09-23 19:32:26 +0200

    removed dead routes

--- main.js
+++ main.js
@@ -7,21 +7,8 @@ app.configure(function(){
     app.use(express.static(__dirname + '/public'));
 });
 
-
-app.get('/appreciation', function(req, res){
-	router.renderIndex(req,res);
-});
-
-app.get('/uploads/*', function(req, res){
-	router.renderStaticFile(req,res);
-});
-
 app.post('/appreciation', function(req, res){
 	router.saveAppreciationRequest(req,res);
-});
-
-app.get('/', function(req, res){
-	router.renderIndex(req,res);
 });
 
 app.listen(8042);
