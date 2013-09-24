752fb6dbecf9a94488d1095e9a060769fbba2df4
peny <petter@pttr.se>
2013-09-24 16:46:26 +0000

    added variable port

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var router = require('./routes/main.js');
 var app = express();
+
+var PORT = 8042;
+
 app.configure(function(){
     app.use(express.bodyParser());
     app.use('/media', express.static(__dirname + '/media'));
-    app.use(express.static(__dirname + '/public'));
+    app.use('/public', express.static(__dirname + '/public'));
+    app.use('/', express.static(__dirname + '/public'));
 });
 
 app.post('/appreciation', function(req, res){
 	router.saveAppreciationRequest(req,res);
 });
 
-app.listen(8042);
+app.listen(PORT);
+console.log('server running at port '+PORT);
