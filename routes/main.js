f348c538cbc0fe4d4c40636fcdd281c478fc1499
peny <petter@pttr.se>
2013-09-25 15:22:20 +0000

    download image from tictail if just a imageurl is sent

--- routes/main.js
+++ routes/main.js
@@ -1,23 +1,37 @@
 var fs = require('fs');
 
+var request = require('request');
+
 var mechaturk = require('../lib/mechaturk.js');
 var tictail = require('../lib/tictail.js');
 var db = require('../lib/db.js');
 
+
 function saveAppreciationRequest(req, res){
   var _this = this;
-  var filetype = req.files.images.name.match(/\..{2,5}$/);
-  var imagePath = "media/"+((new Date()).getTime())+filetype;
-  fs.readFile(req.files.images.path, function (err, data) {
-    fs.writeFile(imagePath, data, function (err) {
+  var imagePath = '';
+
+  //Download image from tictail or get image from request
+  if(req.body.imageurl){
+    var filetype = req.body.imageurl.match(/\..{2,5}$/);
+    imagePath = "media/"+((new Date()).getTime())+filetype;
+    request(req.body.imageurl).pipe(fs.createWriteStream(imagePath));
+  } else {
+    var filetype = req.files.images.name.match(/\..{2,5}$/);
+    imagePath = "media/"+((new Date()).getTime())+filetype;
+    fs.readFile(req.files.images.path, function (err, data) {
+      fs.writeFile(imagePath, data, function (err) {
+      });
     });
-  });
+  }
+
   var data = {
     imageurl: imagePath,
     accesstoken: req.body.accesstoken,
     storeid: req.body.storeid,
     storedashboardurl: req.body.storedashboardurl
   };
+
   db.getQuota(data.storeid);
   tictail.getUser({accessToken: req.body.accesstoken}, function(err,user){
     if(!err){
